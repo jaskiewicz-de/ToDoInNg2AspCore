c85ba8c6b97916e79cea98f1d113b0c5cc91b424
Lukasz Jaskiewicz <dev@jaskiewicz.de>
2016-09-24 00:27:33 +0200

    [cleanup]

--- src/ToDoInNg2AspCore/Program.cs
+++ src/ToDoInNg2AspCore/Program.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-
-namespace ToDoInNg2AspCore
+﻿namespace ToDoInNg2AspCore
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Hosting;
+
     public class Program
     {
         public static void Main(string[] args)
